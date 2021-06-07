cb481cb4d640cf0d73ddc8ff0bc028c888041f14
betawxy <betawxy@gmail.com>
2021-06-06 18:02:48 -0700

    location page layout

--- lib/screens/LocationScreen.dart
+++ lib/screens/LocationScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import '../services/location.dart';
 
 class LocationScreen extends StatefulWidget {
+  static const route = '/location';
+
   const LocationScreen({Key? key, required this.loc}) : super(key: key);
   final Location loc;
 
@@ -13,8 +15,28 @@ class LocationScreen extends StatefulWidget {
 class _LocationScreenState extends State<LocationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(widget.loc.toString()),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.location_on),
+                Icon(Icons.location_city),
+              ],
+            ),
+            Text('9^ C'),
+            Text(
+              'You\'ll need a and b in c!',
+              textAlign: TextAlign.end,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
